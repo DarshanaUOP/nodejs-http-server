a128c90078facbf994bfee2ca932248dcc3bc1df
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-09-25 00:10:47 +0530

    add response to visible in browser

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var http = require("http");
 var server = http.createServer(function(req,res) {
 	console.log("Request : "+req.url);
 	res.writeHead(200,{'Content-Type' : 'text/plain'});
-	res.end();
+	res.end("Darshana Ariyarathna");
 });
 
 var  serverIP = '127.0.0.1';
