c4fc6f80d5f838ea0ae247054debc9f99d2b2bde
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-09-24 23:22:03 +0530

    node is creating simple server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var server = http.createServer(function(req,res) {
+	console.log("Request : "+req.url);
+	res.writeHead(200,{'Content-Type' : 'text/plain'});
+	res.end();
+});
+
+var  serverIP = '127.0.0.1';
+var port = 3000;
+server.listen(port,serverIP);
