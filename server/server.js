394235ee34e33a0ea004759110b7097f29ff38ad
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-09-25 20:33:45 +0530

    change server with basic routing statergies in nodejs

--- server/server.js
+++ server/server.js
@@ -3,10 +3,23 @@ var fs = require("fs");
 
 var server = http.createServer(function(req,res) {
 	console.log("Request : "+req.url);
-	res.writeHead(200,{'Content-Type' : 'text/html'});
 
-	var indexPage = fs.createReadStream('../index.html',"utf-8");
-	indexPage.pipe(res);
+	if(req.url == '/' || req.url == '/home'){
+		res.writeHead(200,{'Content-Type' : 'text/html'});
+		var indexPage = fs.createReadStream('../index.html',"utf-8");
+		indexPage.pipe(res);	
+	}else if(req.url == '/api'){
+		res.writeHead(200,{"Content-Type":"application/json"});
+		var jsonResponse = {
+			'a' : 1,
+			'b' : 'my world'
+		};
+		res.end(JSON.stringify(jsonResponse));
+	}else{
+		res.writeHead(200,{'Content-Type' : 'text/html'});
+		var indexPage = fs.createReadStream('../404.html',"utf-8");
+		indexPage.pipe(res);
+	}
 });
 
 var  serverIP = '127.0.0.1';
