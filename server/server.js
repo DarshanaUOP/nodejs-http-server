a7e4e746eeec474f80484d32b7d7d4ede6b19d52
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-09-25 17:51:49 +0530

    server updated to serve htnl file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function(req,res) {
 	console.log("Request : "+req.url);
-	res.writeHead(200,{'Content-Type' : 'text/plain'});
-	res.end("Darshana Ariyarathna");
+	res.writeHead(200,{'Content-Type' : 'text/html'});
+	var indexPage = fs.createReadStream(__dirname + '/index.html',"utf-8");
+	indexPage.pipe(res);
 });
 
 var  serverIP = '127.0.0.1';
 var port = 3000;
 server.listen(port,serverIP);
+console.log("Server is running on " + serverIP + ":" + port);
